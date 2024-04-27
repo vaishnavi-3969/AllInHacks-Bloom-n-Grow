d46e7f167ec1ef5cd5b25d85cb3b4e523c65288c
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-04-27 17:48:35 +0530

    app initialized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { About, Home, Landing } from "./pages";
+
 
 function App() {
+  const routes = [
+    { path: '/about', element: <About /> },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element=<Landing /> exact />
+          <Route path="/home" element=<Home /> exact />
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} exact />
+          ))}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Home from "./Home";
+import Landing from "./Landing";
+import About from "./About";
+
+export {
+    Home,
+    Landing,
+    About
+}
